937683a90d5f954aef5cf912173e4282efdf1fbb
Magnus Lindstroem <magnus.lindstrom@tuta.io>
2021-10-01 21:07:02 +0200

    fix build for one rust image

--- build_images.sh
+++ build_images.sh
@@ -22,26 +22,6 @@ build_rust_image() {
   fi
 }
 
-build_rust_webassembly() {
-  set +e
-  docker build \
-    --build-arg uid=$(id -u) \
-    --build-arg user=$USER --build-arg home=/home/user \
-    --tag rust_webassembly ./rust_webassembly/
-  build_status=$?
-  set -e
-
-  if [ "$build_status" -eq 0 ]; then
-    echo
-    echo "### BUILD SUCCESSFUL ###"
-    echo
-  else
-    echo
-    echo "### BUILD FAILED ###"
-    echo
-  fi
-}
-
 setup() {
   echo "Setting up dotfiles.."
   cp ~/.bashrc "./rust_general/"
@@ -58,7 +38,6 @@ teardown() {
 
 main() {
   setup
-  # build_rust_webassembly
   build_rust_image
   teardown
 }
